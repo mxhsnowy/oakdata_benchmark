b86aeb407db9114e6b60a834e3976b170c274295
mxhsnowy <huypr09xyz@gmail.com>
2023-09-24 15:20:44 +0700

    Change util directories

--- object_detection/detectron2/ewc/idk/util.py
+++ object_detection/detectron2/ewc/idk/util.py
@@ -39,8 +39,8 @@ from rcnn import EWCRCNN
 
 from detectron2.modeling.roi_heads.roi_heads import build_roi_heads
 
-cat_fp = '/grogu/user/jianrenw/baseline/release/mapping.json'
-pretrained_fp = '/grogu/user/jianrenw/baseline/release/model_final_b1acc2.pkl'
+cat_fp = '../otherfile/mapping.json'
+pretrained_fp = '/media/data5/users/huymx/OAK Data/oakdata_benchmark/model_final_b1acc2.pkl'
 loading_fp = ''
 
 def returnmap():
